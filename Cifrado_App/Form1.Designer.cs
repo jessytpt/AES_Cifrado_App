203e0e681533b40ad1d408eb2dc47ee729fb4c80
Jessenia Toapanta <jessytpt.dev@gmail.com>
2021-06-29 19:39:19 -0500

    cifrado/descifrado por input y cifrado de archivo

--- Cifrado_App/Form1.Designer.cs
+++ Cifrado_App/Form1.Designer.cs
@@ -29,13 +29,168 @@ namespace Cifrado_App
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtInputData = new System.Windows.Forms.TextBox();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.txtEncryptData = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.btnDesEncrypt = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.btnDesEncryptFile = new System.Windows.Forms.Button();
+            this.btnEncryptFile = new System.Windows.Forms.Button();
+            this.btnBrowser = new System.Windows.Forms.Button();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtInputData
+            // 
+            this.txtInputData.Location = new System.Drawing.Point(37, 40);
+            this.txtInputData.Name = "txtInputData";
+            this.txtInputData.Size = new System.Drawing.Size(245, 23);
+            this.txtInputData.TabIndex = 0;
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(37, 83);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(90, 23);
+            this.btnEncrypt.TabIndex = 1;
+            this.btnEncrypt.Text = "Cifrar";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // txtEncryptData
+            // 
+            this.txtEncryptData.Location = new System.Drawing.Point(37, 127);
+            this.txtEncryptData.Multiline = true;
+            this.txtEncryptData.Name = "txtEncryptData";
+            this.txtEncryptData.Size = new System.Drawing.Size(245, 143);
+            this.txtEncryptData.TabIndex = 2;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(16, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(613, 332);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.btnDesEncrypt);
+            this.tabPage1.Controls.Add(this.txtInputData);
+            this.tabPage1.Controls.Add(this.btnEncrypt);
+            this.tabPage1.Controls.Add(this.txtEncryptData);
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(605, 304);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // btnDesEncrypt
+            // 
+            this.btnDesEncrypt.Location = new System.Drawing.Point(178, 83);
+            this.btnDesEncrypt.Name = "btnDesEncrypt";
+            this.btnDesEncrypt.Size = new System.Drawing.Size(90, 23);
+            this.btnDesEncrypt.TabIndex = 3;
+            this.btnDesEncrypt.Text = "Decifrar";
+            this.btnDesEncrypt.UseVisualStyleBackColor = true;
+            this.btnDesEncrypt.Click += new System.EventHandler(this.btnDesEncrypt_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.btnDesEncryptFile);
+            this.tabPage2.Controls.Add(this.btnEncryptFile);
+            this.tabPage2.Controls.Add(this.btnBrowser);
+            this.tabPage2.Controls.Add(this.txtFilePath);
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(605, 304);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btnDesEncryptFile
+            // 
+            this.btnDesEncryptFile.Location = new System.Drawing.Point(168, 52);
+            this.btnDesEncryptFile.Name = "btnDesEncryptFile";
+            this.btnDesEncryptFile.Size = new System.Drawing.Size(90, 23);
+            this.btnDesEncryptFile.TabIndex = 5;
+            this.btnDesEncryptFile.Text = "Decifrar";
+            this.btnDesEncryptFile.UseVisualStyleBackColor = true;
+            this.btnDesEncryptFile.Click += new System.EventHandler(this.btnDesEncryptFile_Click);
+            // 
+            // btnEncryptFile
+            // 
+            this.btnEncryptFile.Location = new System.Drawing.Point(27, 52);
+            this.btnEncryptFile.Name = "btnEncryptFile";
+            this.btnEncryptFile.Size = new System.Drawing.Size(90, 23);
+            this.btnEncryptFile.TabIndex = 4;
+            this.btnEncryptFile.Text = "Cifrar";
+            this.btnEncryptFile.UseVisualStyleBackColor = true;
+            this.btnEncryptFile.Click += new System.EventHandler(this.btnEncryptFile_Click);
+            // 
+            // btnBrowser
+            // 
+            this.btnBrowser.Location = new System.Drawing.Point(297, 13);
+            this.btnBrowser.Name = "btnBrowser";
+            this.btnBrowser.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowser.TabIndex = 1;
+            this.btnBrowser.Text = "Browser";
+            this.btnBrowser.UseVisualStyleBackColor = true;
+            this.btnBrowser.Click += new System.EventHandler(this.btnBrowser_Click);
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(27, 14);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(264, 23);
+            this.txtFilePath.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtInputData;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.TextBox txtEncryptData;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button btnDesEncrypt;
+        private System.Windows.Forms.Button btnDesEncryptFile;
+        private System.Windows.Forms.Button btnEncryptFile;
+        private System.Windows.Forms.Button btnBrowser;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- Cifrado_App/Form1.cs
+++ Cifrado_App/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,10 +14,274 @@ namespace Cifrado_App
 {
     public partial class Form1 : Form
     {
+        byte[] key;
+        byte[] IV;
+        string encryptData;
+        byte[] encrypted;
+        byte[] encryptedFile;
+        byte[] keyFile;
+        byte[] IVFile;
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            string data = txtInputData.Text;
+            EncryptAesManaged(data);
+            txtInputData.Text = "";
+        }
+          void EncryptAesManaged(string raw)
+        {
+            try
+            {
+                // Create Aes that generates a new key and initialization vector (IV).    
+                // Same key must be used in encryption and decryption    
+                using (AesManaged aes = new AesManaged())
+                {
+                    key = aes.Key;
+                    IV = aes.IV;
+                    // Encrypt string    
+                    encrypted = Encrypt(raw, key, IV);
+                    // Print encrypted string    
+                    //Console.WriteLine($ "Encrypted data: {System.Text.Encoding.UTF8.GetString(encrypted)}");
+                    encryptData = Encoding.UTF8.GetString(encrypted);
+                    Console.WriteLine(encryptData);
+                    txtEncryptData.Text = encryptData;
+                    // Decrypt the bytes to a string.    
+                    //string decrypted = Decrypt(encrypted, aes.Key, aes.IV);
+                    // Print decrypted string. It should be same as raw data    
+                    //Console.WriteLine($ "Decrypted data: {decrypted}");
+                }
+            }
+            catch (Exception exp)
+            {
+                //Console.WriteLine(exp.Message);
+            }
+            //Console.ReadKey();
+        }
+        static byte[] Encrypt(string plainText, byte[] Key, byte[] IV)
+        {
+            byte[] encrypted;
+            // Create a new AesManaged.    
+            using (AesManaged aes = new AesManaged())
+            {
+                // Create encryptor    
+                ICryptoTransform encryptor = aes.CreateEncryptor(Key, IV);
+                // Create MemoryStream    
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    // Create crypto stream using the CryptoStream class. This class is the key to encryption    
+                    // and encrypts and decrypts data from any given stream. In this case, we will pass a memory stream    
+                    // to encrypt    
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                    {
+                        // Create StreamWriter and write data to a stream    
+                        using (StreamWriter sw = new StreamWriter(cs))
+                            sw.Write(plainText);
+                        encrypted = ms.ToArray();
+                    }
+                }
+            }
+            // Return encrypted data    
+            return encrypted;
+        }
+        static string Decrypt(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            string plaintext = null;
+            // Create AesManaged    
+            using (AesManaged aes = new AesManaged())
+            {
+                // Create a decryptor    
+                ICryptoTransform decryptor = aes.CreateDecryptor(Key, IV);
+                // Create the streams used for decryption.    
+                using (MemoryStream ms = new MemoryStream(cipherText))
+                {
+                    // Create crypto stream    
+                    using (CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                    {
+                        // Read crypto stream    
+                        using (StreamReader reader = new StreamReader(cs))
+                            plaintext = reader.ReadToEnd();
+                    }
+                }
+            }
+            return plaintext;
+        }
+
+        private void btnDesEncrypt_Click(object sender, EventArgs e)
+        {
+            string dataEncrypt = txtEncryptData.Text;
+            using (AesManaged aes = new AesManaged())
+            {
+                txtInputData.Text= Decrypt(encrypted, key, IV);
+
+            }
+        }
+
+        private void btnBrowser_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                InitialDirectory = @"C:\Users\Jessenia-PC",
+                Title = "Browse Text Files",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "txt",
+                Filter = "txt files (*.txt)|*.txt",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtFilePath.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void btnEncryptFile_Click(object sender, EventArgs e)
+        {
+            string FilePath = txtFilePath.Text;
+            EncryptAesManagedFile(FilePath);
+        }
+
+        void EncryptAesManagedFile(string path)
+        {
+            try
+            {
+                // Create Aes that generates a new key and initialization vector (IV).    
+                // Same key must be used in encryption and decryption    
+                using (AesManaged aes = new AesManaged())
+                {
+                    keyFile = aes.Key;
+                    IVFile = aes.IV;
+                    byte[] plainContent = File.ReadAllBytes(path);
+                    // Encrypt string    
+                    encryptedFile = EncryptFile(plainContent, keyFile, IVFile);
+                   
+                }
+            }
+            catch (Exception exp)
+            {
+                //Console.WriteLine(exp.Message);
+            }
+            //Console.ReadKey();
+        }
+
+        static byte[] EncryptFile(byte[] plainText, byte[] Key, byte[] IV)
+        {
+            byte[] encrypted;
+            // Create a new AesManaged.    
+            using (AesManaged aes = new AesManaged())
+            {
+                // Create encryptor    
+                ICryptoTransform encryptor = aes.CreateEncryptor(Key, IV);
+                // Create MemoryStream    
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    // Create crypto stream using the CryptoStream class. This class is the key to encryption    
+                    // and encrypts and decrypts data from any given stream. In this case, we will pass a memory stream    
+                    // to encrypt    
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                    {
+                        // Create StreamWriter and write data to a stream    
+                        using (StreamWriter sw = new StreamWriter(cs))
+                            sw.Write(plainText);
+                        encrypted = ms.ToArray();
+                        //Escribir lo que esta encriptado
+                        string filePath = @"C:\Users\Jessenia-PC\Cifrado\textoCifrado.txt";
+                        File.WriteAllBytes(filePath, encrypted);
+                    }
+                }
+            }
+            // Return encrypted data    
+            return encrypted;
+        }
+
+        private void btnDesEncryptFile_Click(object sender, EventArgs e)
+        {
+            
+            string FilePath = txtFilePath.Text;
+            byte[] plainContent = File.ReadAllBytes(FilePath);
+            string texto = Encoding.UTF8.GetString(plainContent);
+            
+            
+            using (AesManaged aes = new AesManaged())
+            {
+                string texto1 = DecryptFile(encryptedFile, keyFile, IVFile);
+                file(texto1, FilePath);
+               // DecryptFile(FilePath, keyFile, IVFile);
+
+            }
+        }
+        public void file(string texto, string path) {
+            // Write file using StreamWriter  
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                writer.WriteLine(texto);
+            }
+        }
+        public static async Task ExampleAsync( string texto)
+        {
+            string filePath = @"C:\Users\Jessenia-PC\Cifrado\textoDescifrado.txt";
+            string text =
+                "A class is the most powerful data type in C#. Like a structure, " +
+                "a class defines the data and behavior of the data type. ";
+
+            await File.WriteAllTextAsync(filePath, texto);
+        }
+        static string DecryptF(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            string plaintext = null;
+            // Create AesManaged    
+            using (AesManaged aes = new AesManaged())
+            {
+                // Create a decryptor    
+                ICryptoTransform decryptor = aes.CreateDecryptor(Key, IV);
+                // Create the streams used for decryption.    
+                using (MemoryStream ms = new MemoryStream(cipherText))
+                {
+                    // Create crypto stream    
+                    using (CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                    {
+                        // Read crypto stream    
+                        using (StreamReader reader = new StreamReader(cs))
+                            plaintext = reader.ReadToEnd();
+                    }
+                }
+            }
+            return plaintext;
+        }
+
+        static string DecryptFile(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            string plaintext = null;
+            // Create AesManaged    
+            using (AesManaged aes = new AesManaged())
+            {
+                // Create a decryptor    
+                ICryptoTransform decryptor = aes.CreateDecryptor(Key, IV);
+                // Create the streams used for decryption.    
+                using (MemoryStream ms = new MemoryStream(cipherText))
+                {
+                    // Create crypto stream    
+                    using (CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                    {
+                        // Read crypto stream    
+                        using (StreamReader reader = new StreamReader(cs))
+                            plaintext = reader.ReadToEnd();
+                    }
+                }
+            }
+            return plaintext;
+        }
+
 
     }
 }
